6768d6cc2f29446c5a0463e804f1f88bb8fe52a1
Ben Nguyen <bennguyen@Bens-MacBook-Pro-3.local>
2024-10-04 10:57:19 -0500

    Address port issue

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -10,9 +10,9 @@ resource "aws_security_group" "web_app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "HTTP - Incoming 443"
-    from_port   = 443
-    to_port     = 443
+    description = "HTTP - Incoming 80"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
